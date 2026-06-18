0c3c7a161fa7bee02a068032a34f4d1606b46268
QAIS-CS <qaismabdalsamad@gmail.com>
2026-06-18 15:39:17 +0300

    Add files via upload

---
+++ Fe.java
@@ -0,0 +1,16 @@
+package fe;
+
+/**
+ *
+ * @author qassa
+ */
+public class Fe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
