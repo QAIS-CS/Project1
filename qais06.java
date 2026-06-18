2faddf217d190cdd2eeae0994dd5b4c1ecf6ef55
QAIS-CS <qaismabdalsamad@gmail.com>
2026-06-18 16:13:26 +0300

    Add files via upload

---
+++ qais06.java
@@ -0,0 +1,35 @@
+
+package javaapplication3;
+import java.util.Scanner;
+
+public class JavaApplication3 {
+static Scanner in=new Scanner (System.in);
+     
+    public static void main(String[] args) {
+        String z=done();
+        System.out.println("height="+z);
+        
+       
+    }
+        public static String done() {
+        double h,total=0;
+        String s="";
+        for(int i=1;i<=6;i++){
+            System.out.println("read hight for tree="+i);
+            h=in.nextDouble();
+            total=total+h;
+        }
+            System.out.println("total height="+total);
+            if(total<100)
+                s="low height";
+            else if(total>=100 && total<=500)
+                s="normal height ";
+            else if (total>500)
+                s="over height";
+            return s;
+        }
+}
+
+
+          
+
