097a984de2491d4a18315645d47621e3cbb41f1d
QAIS-CS <qaismabdalsamad@gmail.com>
2026-06-18 16:14:40 +0300

    Add files via upload

---
+++ avg.java
@@ -0,0 +1,27 @@
+package javaapplication13;
+
+import java.util.Scanner;
+
+
+public class JavaApplication13 {
+
+    
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("pleas enter your number : ");
+        System.out.println("====================================");
+        double x1=in.nextDouble();
+        System.out.println("====================================");
+        double x2=in.nextDouble();
+        System.out.println("====================================");
+        double x3=in.nextDouble();
+        System.out.println("======>...this is your avareg...<======");
+        double avg=x1+x2+x3;
+        System.out.println("avg"+avg);
+        
+        
+    }
+
+
+
+}
